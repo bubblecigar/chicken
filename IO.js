b442fe3b08158dddfb0bc44500646e373322d2fb
王瑞瑜 <roywang@h2o-team.com>
2021-06-07 19:54:52 +0800

    change text

--- IO.js
+++ IO.js
@@ -1,7 +1,7 @@
 
 const mount = io => {
   io.on('connection', function (socket) {
-    io.emit('notify', { user: { userName: 'system' }, message: 'new user' })
+    io.emit('notify', { user: { userName: 'system' }, message: 'Someone enter the room' })
 
     socket.on('send', message => {
       socket.broadcast.emit('notify', message)
