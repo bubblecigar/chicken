908bcfdc638c92df18b5438cc9a6ae2576a9c37c
王瑞瑜 <roywang@h2o-team.com>
2021-06-07 16:58:57 +0800

    export io

--- server.js
+++ server.js
@@ -6,18 +6,22 @@ var io = require('socket.io').listen(server);
 app.use('/', express.static(__dirname + '/dist'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(process.env.PORT || 8081, function () {
-    console.log('Listening on ' + server.address().port);
+  console.log('Listening on ' + server.address().port);
 });
 
 io.on('connection', function (socket) {
-    socket.on('send', message => {
-        io.emit('notify', message)
-    })
+  socket.on('send', message => {
+    io.emit('notify', message)
+  })
 
-    socket.on('disconnect', function () {
-    });
+  socket.on('disconnect', function () {
+  });
 });
+
+module.exports = {
+  io
+}
