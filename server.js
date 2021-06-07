4a062fea3740266559bbc42b682e0bb09206d7dc
王瑞瑜 <roywang@h2o-team.com>
2021-06-07 20:13:19 +0800

    update socket.io

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io').listen(server);
+const io = require('socket.io')(server);
 const { mount } = require('./IO.js')
 
 
