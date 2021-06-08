36465336d9afcb168e80469eaba1ae54c89d80c7
王瑞瑜 <roywang@h2o-team.com>
2021-06-08 12:34:01 +0800

    add lodash

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -41155,18 +41155,6 @@ var _ChessBox = require("./ChessBox");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 function _templateObject2() {
   var data = _taggedTemplateLiteral(["\n  display: flex;\n  background-color: white;\n  justify-content: center;\n  align-items: center;\n"]);
 
@@ -41197,11 +41185,7 @@ var Chessboard = function Chessboard() {
   var _React$useContext = _react.default.useContext(_app.GlobalContext),
       gameObject = _React$useContext.gameObject;
 
-  var _onDrop = function onDrop(e, _ref) {
-    var _ref2 = _slicedToArray(_ref, 2),
-        i = _ref2[0],
-        j = _ref2[1];
-
+  var _onDrop = function onDrop(e, to) {
     var json = e.dataTransfer.getData('application/json');
 
     var _JSON$parse = JSON.parse(json),
@@ -41211,8 +41195,12 @@ var Chessboard = function Chessboard() {
     var action = {
       chess: chess,
       from: from,
-      to: [i, j]
+      to: to
     };
+
+    if (from && from[0] === to[0] && from[1] === to[1]) {
+      return; // in the same cell
+    }
 
     _app.socket.emit('move-chess', action);
   };
