0cfd1a34ccb0e8e7a1f8048d4c4b2904fba57fd9
王瑞瑜 <roywang@h2o-team.com>
2021-06-08 13:05:53 +0800

    game panel style adjust

--- js/GamePanel.js
+++ js/GamePanel.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import { GlobalContext, socket } from './app'
 
+const GamePanelStyle = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  border: 1px solid black;
+`
 const GamePanel = () => {
   const { gameObject } = React.useContext(GlobalContext)
   const onReset = () => {
     socket.emit('reset-chessboard')
   }
   return gameObject ? (
-    <div>
+    <GamePanelStyle>
       <div>
         {gameObject.status}
         <button onClick={onReset}>reset</button>
@@ -24,7 +31,12 @@ const GamePanel = () => {
           )
         }
       </div>
-    </div>
+      <div>
+        players:
+        <button>sit red</button>
+        <button>sit blue</button>
+      </div>
+    </GamePanelStyle>
   ) : null
 }
 
