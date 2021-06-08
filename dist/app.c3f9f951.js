d73c8c15b6bf1bb6d239956a519d7adacc5c60b3
王瑞瑜 <roywang@h2o-team.com>
2021-06-08 17:15:29 +0800

    add NES.css

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -41416,7 +41416,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50940" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61206" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
